52b7ec0019e3c8335c5a9cca2eac734ebab0d6f2
a17fernando <a17fernandobr@iessanclemente.net>
2020-04-15 00:12:03 +0200

    Trabajo Contornos v1

---
+++ Maps.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Maps {
+
+    public static void main(String[] args) {
+        Map<String,Double> hm= new HashMap<>();
+        hm.put("Elías", new Double(1500));
+       //mejor con  autoboxing 
+        hm.put("Román", 1900.0);
+        hm.put("Telma", 2400.0);
+        System.out.println("Sueldo de Román: "+ hm.get("Román")); 
+        System.out.println("Está en el mapa la llave Telma?: "+ hm.containsKey("Telma"));
+        System.out.println("Está en el mapa el valor Telma?: "+ hm.containsValue("Telma"));
+        Double d=1900.0;
+        System.out.println("Está en el mapa el valor 1900?: "+ hm.containsValue(d));
+        d=1899.98; 
+        System.out.println("Está en el mapa el valor 1899.98?: "+ hm.containsValue(d));
+    }
+}
